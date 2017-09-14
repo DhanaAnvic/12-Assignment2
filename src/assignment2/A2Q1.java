d631c0e800c8f630774cf02294f02cd799eea648
fabed2976 <fabed2976@HRH-IC0027343.wrdsb.ca>
2017-09-14 09:44:25 -0400

    A2Q1 Finished

--- src/assignment2/A2Q1.java
+++ src/assignment2/A2Q1.java
@@ -10,13 +10,22 @@ package assignment2;
  */
 public class A2Q1 {
     
+    //create a digitalSum method that returns the sum of the integer
+ 
     public int digitalSum (int n){
         
+        //create an if statement that is the 10 is greater than the number
         if(n < 10){
+            //it will go back to the number itself
             return n;
+            // and the else statement is when the number is greater than the number
+            
         }else{
+            //create a variable to find the remainder of the number when you divide it by 10
             int right = n % 10;
+            //create a varaible to find the quotient of the number by dividing it by 10
             int left = n / 10;
+            //create a return statement that returns the quotient and it to the remainder
             return digitalSum(left) + right;
     }
     }
@@ -24,6 +33,7 @@ public class A2Q1 {
     
     public static void main(String[] args) {
         
+        //create a varibel to test the boundaries
         A2Q1 test =  new A2Q1();
         System.out.println("digitalsum (126)");
         int digitalSum = test.digitalSum(126);
