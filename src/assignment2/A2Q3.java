465e147a3f9b3b48dd9f5141e94159212ed84e9d
fabed2976 <fabed2976@HRH-IC0027343.wrdsb.ca>
2017-09-15 10:06:36 -0400

    A2Q3 finished

--- src/assignment2/A2Q3.java
+++ src/assignment2/A2Q3.java
@@ -10,19 +10,39 @@ package assignment2;
  */
 public class A2Q3 {
     
+    //create a mathod called triangle that returns the total number of the triangles in a row
+    
    public int triangle (int num){
+       
+       //create an if statment that if 2 is greater than the num
        if (num < 2){
+           //now the number will return itself
            return num;
+           //create an else statement if the num is greater than 2
        }else{
+           //it will return the num and add the triangle with the num subtracting it by 1
+           // to know how many total number of the triangles
            return num + triangle(num-1);
        }
    }
     
     public static void main(String[] args) {
-        // TODO code application logic here
+        //create a variable to test the boundaries
         A2Q3 test = new A2Q3 ();
         System.out.println("triangle(0)");
         int triangle = test.triangle(0);
         System.out.println(triangle);
+        
+        System.out.println("triangle(1)");
+        int triangle1 = test.triangle(1);
+        System.out.println(triangle1);
+        
+        System.out.println("triangle(2)");
+        int triangle2 = test.triangle(2);
+        System.out.println(triangle2);
+        
+        System.out.println("triangle(3)");
+        int triangle3 = test.triangle(3);
+        System.out.println(triangle3);
     }
 }
