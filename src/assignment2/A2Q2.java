eeca09e84908e4ce2a80d64b3b399430ac793625
fabed2976 <fabed2976@HRH-IC0027343.wrdsb.ca>
2017-09-15 10:06:19 -0400

    A2Q2 finished

--- src/assignment2/A2Q2.java
+++ src/assignment2/A2Q2.java
@@ -9,32 +9,45 @@ package assignment2;
  * @author fabed2976
  */
 public class A2Q2 {
+    
+    //create a digitalSum  method that returns the sum of the integer 
 
     public int digitalSum(int num) {
+        //create an if statement that if 10 is greater than the number
         if (num < 10) {
+             //it will go back to the number itself
             return num;
+            // and the else statement is when the number is greater than 10
         } else {
+            //create a variable to find the remainder of the number when you divide it by 10
             int remainder = num % 10;
+             //create a varaible to find the quotient of the number by dividing it by 10
             int quotient = num / 10;
+             //create a return statement that returns the quotient and it to the remainder
             return digitalSum(quotient) + remainder;
 
         }
     }
 
-    
+    //create digitalRoot that return the digtial root of n
     public int digitalRoot(int n) {
-
+        
+        //create an if statement if 10 is greater than the number
         if (n < 10) {
+            //the number will return itself
             return n;
+            //create an else statement is when the number is greater than 10
         }else{
+            //try to make the n into a digitalSum to go over the digital sum method again
         n = digitalSum(n);
+        // now return the digitalroot to digitalsum n
         return digitalRoot(digitalSum(n));
     }
     }
     
     
     public static void main(String[] args) {
-        // TODO code application logic here
+        // create a variable to print out the boundaries
         A2Q2 test = new A2Q2();
         
         System.out.println("digital (2019)");
