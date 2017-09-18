1420c8e0f454fd6a04ba80c08bb7ed473ac52772
fabed2976 <fabed2976@HRH-IC0027295.wrdsb.ca>
2017-09-18 12:46:26 -0400

    A2Q5 finished

--- src/assignment2/A2Q5.java
+++ src/assignment2/A2Q5.java
@@ -10,15 +10,22 @@ package assignment2;
  */
 public class A2Q5 {
     
+    //create a method name binaryConvert to return the integer num as a binary
+    
    public void binaryConvert(int num){
        
+       //create an if statement that if num is greater than 0
        if (num > 0){
+           //call in the method and divide the num by 2
            binaryConvert( num / 2);
-           System.out.print(num % 2 + " " );
+           //print out the remainder
+           System.out.print(num % 2);
        }
        }
        
     public static void main(String[] args) {
+        
+        //testing out the boundaries
         A2Q5 test = new A2Q5();
         System.out.println("binaryconvert(156)");
         test.binaryConvert(156);
