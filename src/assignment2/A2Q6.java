5d166a064f9afe3d2048b80c2dcd43f0f4782918
fabed2976 <fabed2976@HRH-IC0027291.wrdsb.ca>
2017-09-19 14:17:55 -0400

    A2Q6 finisheed

--- src/assignment2/A2Q6.java
+++ src/assignment2/A2Q6.java
@@ -11,16 +11,24 @@ package assignment2;
 public class A2Q6 {
     
   public String convert (int n, int b){
+      //create a new empty String
+      String letters = "";
       
+      //create an array to store in the string that has all the numbers and letters
+      String[] answer = {"0", "1", "2","3", "4", "5", "6" , "7", "8", "9", "A", "B", "C", "D" , "E" , "F"};
+      
+      //create a base case
       //create an if statement that is n is equals 0
       if(n == 0) {
-          //return to nothing
-          return"";
-          //create an if statement that if n is not equal to 0
-      }else {
-          //return to the main method and divid the integer by the base and add to the remainder of the integer and base together
-          return convert(n / b, b) + (n % b);
+          //return to string named letters
+          return letters;
       }
+      //replace the letters or stringinto the answer that has all the numbers and letters with the remainder
+      letters= answer[ n % b];
+      
+      //this returns the method by taking in the positive integer and the base and then dividing n by b 
+      //printing the letters with it
+      return convert( n / b, b)+ letters;
       
 }
 
@@ -28,7 +36,7 @@ public class A2Q6 {
         
         //test out the boundaries
         A2Q6 test = new A2Q6();
-        String Convertest = test.convert(1000, 16);
+        String Convertest = test.convert(1000,16);
         System.out.println(Convertest);
     }
 }
